0b8b10bf88a4e7631a6b5c96ec9b01693f68f7aa
lRENyaaa <92320175+lRENyaaa@users.noreply.github.com>
2022-05-16 18:58:52 +0800

    添加了一些effect

---
+++ src/main/java/com/renyuansurvival/quickshopskript/effect/QuickShopPriceSet.java
@@ -0,0 +1,55 @@
+package com.renyuansurvival.quickshopskript.effect;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import com.renyuansurvival.quickshopskript.QuickshopSkript;
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.maxgamer.quickshop.api.shop.Shop;
+
+@Name("Set QuickShop price")
+@Description("Set QuickShop price from block")
+public class QuickShopPriceSet extends Effect {
+
+    static {
+        Skript.registerEffect(QuickShopPriceSet.class, "shop %block%['s] price set to %string%");
+    }
+
+    private Expression<Block> block;
+    private Expression<String> price;
+
+    @Override
+    protected void execute(@NotNull Event e) {
+        Shop shop = QuickshopSkript.getShop(block.getSingle(e));
+        Double shopPrice = null;
+        if(price.getSingle(e) != null) {
+            try {
+                shopPrice = Double.valueOf(price.getSingle(e));
+            } catch (NumberFormatException error){
+                shopPrice = null;
+            }
+        }
+        if (shop != null && shopPrice != null ) shop.setPrice(shopPrice);
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean b) {
+        return String.valueOf(QuickshopSkript.getShop(block.getSingle(e)));
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean init(Expression<?>[] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parser) {
+        block = (Expression<Block>) exprs[0];
+        price = (Expression<String>) exprs[1];
+        return true;
+    }
+}
---
+++ src/main/java/com/renyuansurvival/quickshopskript/effect/SellShopSet.java
@@ -0,0 +1,41 @@
+package com.renyuansurvival.quickshopskript.effect;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import com.renyuansurvival.quickshopskript.QuickshopSkript;
+import org.bukkit.block.Block;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.maxgamer.quickshop.api.shop.Shop;
+import org.maxgamer.quickshop.api.shop.ShopType;
+
+public class SellShopSet extends Effect {
+
+    static {
+        Skript.registerEffect(SellShopSet.class, "set shop %block% [to] selling");
+    }
+
+    private Expression<Block> block;
+
+    @Override
+    protected void execute(@NotNull Event e) {
+        Shop shop = QuickshopSkript.getShop(block.getSingle(e));
+        if (shop != null) shop.setShopType(ShopType.SELLING);
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean b) {
+        return String.valueOf(QuickshopSkript.getShop(block.getSingle(e)));
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean init(Expression<?>[] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parser) {
+        block = (Expression<Block>) exprs[0];
+        return true;
+    }
+}
